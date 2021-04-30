b73fcba689be08658b7145c97cac4a804b6f0f41
beastbernal <beastbernal@gmail.com>
2021-04-29 23:09:55 -0500

    Fixed exam error

--- services/PaymentService.js
+++ services/PaymentService.js
@@ -20,7 +20,7 @@ class PaymentService {
       {
         id: 1234,
         title: name,
-        description: "Celular de Tienda e-commerce",
+        description: "Dispositivo móvil de Tienda e-commerce",
         picture_url: img,
         quantity: parseInt(unit),
         unit_price: parseFloat(price),
@@ -35,8 +35,8 @@ class PaymentService {
         surname: "Landa",
         email: "test_user_83958037@testuser.com",
         phone: {
-          area_code: "1",
-          number: "6050505",
+          area_code: "52",
+          number: "5549737300",
         },
         address: {
           zip_code: "03940",
